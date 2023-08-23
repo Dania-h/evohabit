b8d2a89bd266cb2ed88d92148f7c1f48e4660cf6
Jose Aparicio <jfacademusic@gmail.com>
2023-08-23 19:04:11 -0400

    initialized repo

---
+++ app/(auth)/profile.tsx
@@ -0,0 +1,61 @@
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { useUser } from '@clerk/clerk-expo';
+
+const Profile = () => {
+    const { user } = useUser();
+    const [firstName, setFirstName] = useState(user?.firstName);
+    const [lastName, setLastName] = useState(user?.lastName);
+
+    const onSaveUser = async () => {
+        if (!user) {
+            return;
+        } else {
+            if (firstName === null || lastName === null) {
+                return;
+            } else {
+                try {
+                    // This is not working!
+                    const result = await user.update({
+                        firstName: firstName,
+                        lastName: lastName,
+                    });
+                    console.log('🚀 ~ file: profile.tsx:16 ~ onSaveUser ~ result:', result);
+                } catch (e) {
+                    console.log('🚀 ~ file: profile.tsx:18 ~ onSaveUser ~ e', JSON.stringify(e));
+                }
+            }
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ textAlign: 'center' }}>
+                Good morning {user?.firstName} {user?.lastName}!
+            </Text>
+
+            <TextInput placeholder="First Name" value={firstName ? firstName : undefined} onChangeText={setFirstName} style={styles.inputField} />
+            <TextInput placeholder="Last Name" value={lastName ? lastName: undefined} onChangeText={setLastName} style={styles.inputField} />
+            <Button onPress={onSaveUser} title="Update account" color={'#6c47ff'}></Button>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40,
+    },
+    inputField: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#6c47ff',
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+});
+
+export default Profile;
